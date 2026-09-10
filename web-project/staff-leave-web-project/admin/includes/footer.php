40e24b4e27a3b6917a4153ce01c42584208cc8e4
daniel09077 <danielokuguni15@gmail.com>
2026-09-10 05:03:22 -0700

    initial commit

---
+++ web-project/staff-leave-web-project/admin/includes/footer.php
@@ -0,0 +1,3 @@
+<div class="footer-wrap pd-20 mb-20 card-box">
+			KPLS Leave System <a href="https://kadunapolytechnic.edu.ng" target="_blank"><span>developed by </span> Group 24</a>
+			</div>
---
+++ web-project/staff-leave-web-project/staff/includes/footer.php
@@ -0,0 +1,3 @@
+<div class="footer-wrap pd-20 mb-20 card-box">
+			KPSLMS Leave System <a href="https://kadunapolytechnic.edu.ng" target="_blank"><span>developed by </span> Group 24</a>
+			</div>
